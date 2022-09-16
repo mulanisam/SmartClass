3ad1c77e610eeb3f68401a0fde76f988767e1d3f
SAMIR MULANI <samirm@cdac.in>
2022-09-16 11:24:47 +0530

    Smart Class Initial Push with SignUp and SignIn

---
+++ src/main/java/com/app/SmartClassApplication.java
@@ -0,0 +1,14 @@
+package com.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
+@CrossOrigin
+@SpringBootApplication
+public class SmartClassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartClassApplication.class, args);
+	}
+
+}
